fc44e1db4b4a09028bf14495ea9c7b1d0a7b6b24
Robert Talos <robert.talos@hogia.se>
2022-12-19 17:34:42 +0100

    NOT WORKING - WIP

---
+++ AoC2022_Day9/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+
+
+class Head
+{
+    private (int x, int y) cordinate;
+
+    public (int x, int y) Cordinate { get => cordinate; set => cordinate = value; }
+}
+class Tail
+{
+    private (int x, int y) cordinate;
+
+    public (int x, int y) Cordinate { get => cordinate; set => cordinate = value; }
+}
